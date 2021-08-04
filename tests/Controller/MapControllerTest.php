f9ce7252cc44b8f1e5d693a1b5056687556ea48e
mathiasDuchossoy <mathias.duchossoy@gmail.com>
2021-08-04 11:13:09 +0200

    [game] :white_check_mark: add test with no game for MapControllerTest

--- tests/Controller/MapControllerTest.php
+++ tests/Controller/MapControllerTest.php
@@ -2,6 +2,8 @@
 
 namespace App\Tests\Controller;
 
+use App\Entity\Game;
+use App\Repository\GameRepository;
 use App\Service\MapService;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
@@ -10,8 +12,14 @@ class MapControllerTest extends WebTestCase
     public function testIndex()
     {
         $client = static::createClient();
-
         $container = static::getContainer();
+
+        $game = $this->createMock(Game::class);
+        $gameRepository = $this->createMock(GameRepository::class);
+        $gameRepository->method('findOneBy')
+            ->willReturn($game);
+
+        $container->set(GameRepository::class, $gameRepository);
 
         $mapService = $this->createMock(MapService::class);
         $mapService->expects($this->once())
@@ -23,5 +31,28 @@ class MapControllerTest extends WebTestCase
 
         $response = $client->getResponse();
         $this->assertResponseIsSuccessful($response);
+    }
+
+    public function testIndexWithoutGame()
+    {
+        $client = static::createClient();
+        $container = static::getContainer();
+
+        $gameRepository = $this->createMock(GameRepository::class);
+        $gameRepository->method('findOneBy');
+
+        $container->set(GameRepository::class, $gameRepository);
+
+        $client->request('GET', '/map');
+
+        $this->assertResponseStatusCodeSame(404);
+        $this->assertResponseHeaderSame('Content-Type', 'application/json');
+
+        $content = $client->getResponse()->getContent();
+        $this->assertJson($content);
+
+        $content = json_decode($content, true);
+
+        $this->assertEquals('no game.', $content);
     }
 }
